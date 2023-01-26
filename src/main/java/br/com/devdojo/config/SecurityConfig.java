e66b335697fb5aec7896461ea802420621dc553b
gabriel <gajsilva@gmail.com>
2023-01-25 23:03:15 -0300

    update

--- src/main/java/br/com/devdojo/config/SecurityConfig.java
+++ src/main/java/br/com/devdojo/config/SecurityConfig.java
@@ -2,21 +2,44 @@ package br.com.devdojo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.servlet.Filter;
 
-@EnableWebSecurity
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
 @Configuration
-public class SecurityConfig extends WebSecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.addFilter()
-                .
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user").password(passwordEncoder().encode("devdojo")).roles("USER").build();
+        UserDetails admin = User.builder()
+                .username("admin").password(passwordEncoder().encode("devdojo")).roles("USER","ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
